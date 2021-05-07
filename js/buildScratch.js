d95a470dabe75f6576762b69b7b0b7038530ec6e
MrVuThanhCong <mr.vtcsharp@gmail.com>
2021-05-07 17:07:12 +0700

    first commit

---
+++ js/buildScratch.js
@@ -0,0 +1,5 @@
+$('#scratch').wScratchPad({
+    size        : 150,          // The size of the brush/scratch.
+    bg          : './assets/img/glass_2.jpg',  // Background (image path or hex color).
+    fg          : './assets/img/glass_1.jpg',  // Foreground (image path or hex color).
+});
